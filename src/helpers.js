806d1b0c538910d26ac715214663b12f016b62b6
Daniel Zelaya <wupaz9@gmail.com>
2018-02-02 16:41:12 -0600

    Add randomUnitVector helper

--- src/helpers.js
+++ src/helpers.js
@@ -2,3 +2,8 @@ export function focusElement(element) {
   if (!element) throw Error('An element must be passed.');
   element.focus();
 }
+
+export const randomUnitVector = () => ({
+  x: Math.round(Math.random()) ? 1 : -1,
+  y: Math.round(Math.random()) ? 1 : -1
+});
--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,4 +1,4 @@
-import { focusElement } from './helpers';
+import { focusElement, randomUnitVector } from './helpers';
 
 describe('focusElement', () => {
   test('should throw error if no element is passed', () => {
@@ -8,5 +8,23 @@ describe('focusElement', () => {
     const fakeElement = { focus: jest.fn() };
     focusElement(fakeElement);
     expect(fakeElement.focus).toHaveBeenCalled();
+  });
+});
+
+describe('randomUnitVector', () => {
+  test('should return a random unit vector', () => {
+    const a = randomUnitVector();
+    const b = randomUnitVector();
+    const c = randomUnitVector();
+    const d = randomUnitVector();
+    const e = randomUnitVector();
+
+    const isUnit = v => v === 1 || v === -1;
+
+    expect(isUnit(a.x) && isUnit(a.y)).toBe(true);
+    expect(isUnit(b.x) && isUnit(b.y)).toBe(true);
+    expect(isUnit(c.x) && isUnit(c.y)).toBe(true);
+    expect(isUnit(d.x) && isUnit(d.y)).toBe(true);
+    expect(isUnit(e.x) && isUnit(e.y)).toBe(true);
   });
 });
